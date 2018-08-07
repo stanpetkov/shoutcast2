9163336fc65808aa7fbc2e868635bd67dc8ef682
s.petkov <s.petkov@cheil.com>
2018-08-07 11:17:04 +0200

    -- updates --

--- js/settings.js
+++ js/settings.js
@@ -1,6 +1,5 @@
 
 // Options
-
 obj.props.ip = 'http://78.129.146.97:7070/'; // your shoutcast 2 ip with port
 obj.props.songSize = 12; // song font size
 obj.props.bandNameSize = 18; // band name font size
@@ -8,6 +7,5 @@ obj.props.borderRound = 20; // border round (default: 20, set it to 0, if you wa
 
 
 // Do not change these options
-
 obj.play();
 obj.options();
